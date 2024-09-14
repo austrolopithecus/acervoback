2bf7f89fbc39d9b91169ffc73a9bc1ab940f5d7a
austrolopithecus <ga.czsan@gmail.com>
2024-09-14 15:29:35 -0300

    Adição de rota de troca de quadrinhos

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/swagger"
 	"github.com/joho/godotenv"
-
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"os"
@@ -24,22 +23,43 @@ import (
 // @name Authorization
 func main() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
-
 	_ = godotenv.Load()
 	app := fiber.New()
 	db.InitDB()
+
+	// Inicializa os repositórios
 	userRepo := repository.NewUserRepo(db.DB)
 	jwtRepo := &repository.JwtRepoImpl{}
 	comicRepo := repository.NewComicRepoImpl(db.DB)
+	exchangeRepo := repository.NewExchangeRepo(db.DB)
+
+	// Inicializa os serviços
 	comicSvc := services.NewComicService(comicRepo)
 	userSvc := services.NewUserService(userRepo, jwtRepo)
+	exchangeSvc := services.NewExchangeService(exchangeRepo, comicRepo, userRepo)
+
+	// Inicializa os handlers
 	userHandler := handlers.NewUserHandler(userSvc)
 	comicHandler := handlers.NewComicHandler(comicSvc)
+	exchangeHandler := handlers.NewExchangeHandler(exchangeSvc)
+
+	// Rotas de usuário
 	app.Post("/user/register", userHandler.Register)
 	app.Post("/user/login", userHandler.Login)
 	app.Get("/user/me", userHandler.JwtMiddleware, userHandler.Me)
-	app.Get("/swagger/*", swagger.HandlerDefault)
+
+	// Rotas de quadrinhos
 	app.Put("/comic", userHandler.JwtMiddleware, comicHandler.CreateComic)
 	app.Get("/comic", userHandler.JwtMiddleware, comicHandler.GetComics)
+
+	// Rotas de troca de quadrinhos
+	app.Post("/exchange", userHandler.JwtMiddleware, exchangeHandler.RequestExchange)
+	app.Post("/exchange/:id/complete", userHandler.JwtMiddleware, exchangeHandler.CompleteExchange)
+
+	// Rota para Swagger
+	app.Get("/swagger/*", swagger.HandlerDefault)
+
+	// Inicia o servidor
 	_ = app.Listen(":3000")
 }
+
