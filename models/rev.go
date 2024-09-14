4000c1294400d9baf667e988e98765d6f3136288
austrolopithecus <ga.czsan@gmail.com>
2024-09-14 17:53:55 -0300

    Correção

--- models/rev.go
+++ models/rev.go
@@ -1,13 +1,10 @@
 package models
 
-import "time"
-
 type Review struct {
-	ID        string    `json:"id" gorm:"primaryKey"`
-	UserID    string    `json:"user_id"`
-	ComicID   string    `json:"comic_id"`
-	Rating    int       `json:"rating"` // rating de 1 a 5
-	Comment   string    `json:"comment"`
-	CreatedAt time.Time `json:"created_at"`
+    ID      string `json:"id" gorm:"primaryKey"`
+    ComicID string `json:"comic_id"`
+    UserID  string `json:"user_id"`
+    Rating  int    `json:"rating"`
+    Comment string `json:"comment"`
 }
 
